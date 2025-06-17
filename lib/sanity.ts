0989d30484147dbb95c35637c75568545c338b29
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2025-06-17 12:04:12 -0300

    Env local e sanity

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "xvi6m0oc",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: "2025-06-14",
   useCdn: true,
 });
