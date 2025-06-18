cf3b20aa82d34a6a7bdb819ee6433b0688b05c4e
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2025-06-18 12:30:45 -0300

    Config auto de sitemap e robots por next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://landings-julia.vercel.app",
+  generateRobotsTxt: true,
+  // outras opções...
+};
+
+module.exports = config;
